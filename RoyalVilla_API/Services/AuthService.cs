cc1e51ab5aa7d03e13928a5c76124f86ba044830
younes-teh <younes.boualam66@gmail.com>
2026-05-12 15:57:15 +0100

    Check Email Uniqueness

--- RoyalVilla_API/Services/AuthService.cs
+++ RoyalVilla_API/Services/AuthService.cs
@@ -1,12 +1,25 @@
-﻿using RoyalVilla_API.Models.DTO;
+﻿using AutoMapper;
+using Microsoft.EntityFrameworkCore;
+using RoyalVilla_API.Data;
+using RoyalVilla_API.Models.DTO;
 
 namespace RoyalVilla_API.Services
 {
     public class AuthService : IAuthService
     {
-        public Task<bool> IsEmailExistsAsync(string email)
+        private readonly ApplicationDbContext _db;
+        private readonly IMapper _mapper;
+        private readonly IConfiguration configuration;
+
+        public AuthService(ApplicationDbContext db, IMapper mapper, IConfiguration configuration)
         {
-            throw new NotImplementedException();
+            _db = db;
+            _mapper = mapper;
+            
+        }
+        public async Task<bool> IsEmailExistsAsync(string email)
+        {
+            return await _db.Users.AnyAsync(u=> u.Email.Equals(email,StringComparison.CurrentCultureIgnoreCase));
         }
 
         public Task<LoginResponseDTO?> LoginAsync(LoginRequestDTO loginRequestDTO)
